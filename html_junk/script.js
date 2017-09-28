45f9bca659ec609dd2e781e3804fe5185bf5df4c
ldominguez721 <luisricodoc@hotmail.com>
2017-09-28 05:01:33 -0400

    Success!!!!!! web3 integration finally works! Thorough googling saves the day

--- html_junk/script.js
+++ html_junk/script.js
@@ -1,11 +1,11 @@
 //Initialize web3
+//var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+
 if(typeof web3 !== 'undefined') {
-	App.web3Provider = web3.currentProvider;
-	web3 = new Web3(web3.currentProvider);
+	web3 = new Web3(new Web3.currentProvider);
 } else {
   // If no injected web3 instance is detected, fallback to the TestRPC.
-	App.web3Provider = new web3.providers.HttpProvider('http://localhost:8545');
-	web3 = new Web3(App.web3Provider);
+	web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 }
 
 var ethAmount = web3.eth.getBalance("0xbf2d51d1ab732f130362891e61a084e3ddba6a45");
