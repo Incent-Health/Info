2b741f65af98e7e1a5427150f0fcbb586ccfeabc
ldominguez721 <luisricodoc@hotmail.com>
2017-09-28 04:48:23 -0400

    Added web3.min.js to webapp, and edited script.js to show balance of deployed contract in nameInput

--- html_junk/script.js
+++ html_junk/script.js
@@ -1,4 +1,15 @@
-var Web3 = require('web3');
+//Initialize web3
+if(typeof web3 !== 'undefined') {
+	App.web3Provider = web3.currentProvider;
+	web3 = new Web3(web3.currentProvider);
+} else {
+  // If no injected web3 instance is detected, fallback to the TestRPC.
+	App.web3Provider = new web3.providers.HttpProvider('http://localhost:8545');
+	web3 = new Web3(App.web3Provider);
+}
+
+var ethAmount = web3.eth.getBalance("0xbf2d51d1ab732f130362891e61a084e3ddba6a45");
+$(".nameInput").attr("value",ethAmount);
 
 $(".submitButton").click(function(){ 
 	console.log("CLICKED");
