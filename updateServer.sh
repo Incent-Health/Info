51e2907ad36a492f699f80a046b9f07b4fe5b3d5
ldominguez721 <luisricodoc@hotmail.com>
2017-09-28 19:22:03 -0400

    Forgot to change dir information in one parameter of script, fixed

--- updateServer.sh
+++ updateServer.sh
@@ -1,3 +1,3 @@
-scp -rp -P 21098 -i ~/.ssh/id_rsa /home/luiserebii/ldominguez721_workspace/IncentHealth_current/html_junk luisjgkr@server115.web-hosting.com:public_html/nautilusblue.xyz/truelab
+scp -rp -P 21098 -i ~/.ssh/id_rsa /home/luiserebii/ldominguez721_workspace/IncentHealth_current/html_junk luisjgkr@server115.web-hosting.com:public_html/sunquyman.xyz/incenthealth/
 printf 'Files uploaded to IncentHealth!\n'
 
