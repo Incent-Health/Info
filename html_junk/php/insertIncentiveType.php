9a43affb0597f26bc6002350596293eab603656e
ldominguez721 <luisricodoc@hotmail.com>
2017-10-02 20:22:48 -0400

    insertIncentHealth.php now works well... now to test if we can jump from a page with onclick and trigger the php running as well, or if we may have to just implement JS/jQuery

--- html_junk/php/insertIncentiveType.php
+++ html_junk/php/insertIncentiveType.php
@@ -1,9 +1,10 @@
 <?php
 
 	//Get info sent from patientscript2S.js
-	$incentivetype=($_POST['incentivetype']);
+	$incentivetype=($_POST['incentiveselect']);
 	$address=($_POST['address']);
-
+	echo $incentivetype;
+	echo "\n" . $address;
 	//Setting up variables
 	$username="luisjgkr_incenthealth";
 	$password="incenthealth404";
@@ -13,7 +14,7 @@
 	//Connect to server with mysqli and select database
 	$mysqli = new mysqli($serverURL,$username,$password,$database);
 
-	$result = $mysqli->query("UPDATE patientData SET incentive_type='" . $incentivetype . "' WHERE uport_address='" . $address . "')");
-	echo $firstname + $result;
+	$result = $mysqli->query("UPDATE patientData SET incentive_type='" . $incentivetype . "' WHERE uport_address='" . $address . "'");
+	echo "reached end of PHP";
 
 ?>
