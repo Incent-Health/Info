e13f3d0dab8e3677a293e7d26ef89587cc0a725f
ldominguez721 <luisricodoc@hotmail.com>
2017-10-02 20:28:20 -0400

    insertIncentHealth.php now works well... now to test if we can jump from a page with onclick and trigger the php running as well, or if we may have to just implement JS/jQuery

---
+++ html_junk/patientscript2.js
@@ -0,0 +1,23 @@
+var incentiveselect;
+
+	incentiveselect = $("incentiveSelect").val();
+	console.log("incentiveselect=" + incentiveselect + "&address=" + getUrlParameter('address'));
+	sendIncentiveSelectToPHP("incentiveselect=" + incentiveselect + "&address=" + getUrlParameter('address'));
+
+$(".submitButton").click(function(){
+	incentiveselect = $("incentiveSelect").val();
+	sendIncentiveSelectToPHP("incentiveselect=" + incentiveselect + "&address=" + getUrlParameter('address'));
+});
+
+function sendIncentiveSelectToPHP(dataInput){ //read about JS Asynchronous nature
+	$.post("php/insertIncentiveType.php", dataInput, function(data){ 
+		console.log(data);
+	});
+};
+
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
---
+++ ldominguez721/insertIncentiveType.php
@@ -0,0 +1,20 @@
+<?php
+
+	//Get info sent from patientscript2S.js
+	$incentivetype=($_POST['incentiveselect']);
+	$address=($_POST['address']);
+	echo $incentivetype;
+	echo "\n" . $address;
+	//Setting up variables
+	$username="luisjgkr_incenthealth";
+	$password="incenthealth404";
+	$serverURL="localhost"; //replace this with localhost for deployment
+	$database="luisjgkr_IncentHealth";
+	//echo $incentivetype;
+	//Connect to server with mysqli and select database
+	$mysqli = new mysqli($serverURL,$username,$password,$database);
+
+	$result = $mysqli->query("UPDATE patientData SET incentive_type='" . $incentivetype . "' WHERE uport_address='" . $address . "'");
+	echo "reached end of PHP";
+
+?>
